bd733cfff3e57bb761808be3e7ebb2ac558071ad
noname2929 <oxygen-xz@example.com>
2020-02-14 12:50:21 +0300

    added SizeTypeNumbers

---
+++ src/Main.java
@@ -0,0 +1,45 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Целые числа:");
+        System.out.println("byte");
+        int byte_min = Byte.MIN_VALUE;
+        System.out.println("Минимальное значение byte: " + byte_min);
+        int byte_max = Byte.MAX_VALUE;
+        System.out.println("Максимальное значение byte: " + byte_max);
+
+        System.out.println("int");
+        int int_min = Integer.MIN_VALUE;
+        System.out.println("Минимальное значение int: " + int_min);
+        int int_max = Integer.MAX_VALUE;
+        System.out.println("Максимальное значение int: " + int_max);
+
+        System.out.println("short");
+        int min_short = Short.MIN_VALUE;
+        System.out.println("Минимальное значение short: " + min_short);
+        int max_short = Short.MAX_VALUE;
+        System.out.println("Максимальное значение short: " + max_short);
+
+        System.out.println("long");
+        double min_long = Long.MIN_VALUE;
+        System.out.println("Минимальное значение long: " + min_long);
+        double max_long = Long.MAX_VALUE;
+        System.out.println("Максимальное значение long: " + max_long);
+
+
+        System.out.println("С плавающей точкой:");
+        System.out.println("float");
+        double min_float = Float.MIN_VALUE;
+        System.out.println("Минимальное значение float: " + min_float);
+        double max_float = Float.MAX_VALUE;
+        System.out.println("Максимальное значение float: " + max_float);
+
+        System.out.println("double");
+        double min_double = Double.MIN_VALUE;
+        System.out.println("Минимальное значение float: " + min_double);
+        double max_double = Double.MAX_VALUE;
+        System.out.println("Максимальное значение float: " + max_double);
+
+    }
+
+}
